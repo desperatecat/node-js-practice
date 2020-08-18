ca07aa1a879de7e35bb94ef32a4febc9e997962d
desperatecat <376198935@qq.com>
2020-08-18 08:01:51 +0800

    response object

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    //write content we want to send back to the browser
+    res.write('<p>hello, ninjas!</p>');
+    res.write('<p>hello again, ninjas!</p>');
+
+    //end the response
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
