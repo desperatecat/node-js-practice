2935b39822b048c18c9e5bb6bebfc38be837cb3e
desperatecat <376198935@qq.com>
2020-08-18 09:47:01 +0800

    redirect

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
-
+    //routing
     let path = './views/';
     switch(req.url) {
         case '/': 
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
             break;
         default: 
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
@@ -30,8 +38,8 @@ const server = http.createServer((req, res) => {
             res.end();
         } else {
             //write content we want to send back to the browser
-            res.write(data);
-            res.end();
+            //res.write(data);
+            res.end(data);
         }
     })
 });
