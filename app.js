a839b7b6df20bc608d883649114869a0a821a064
desperatecat <376198935@qq.com>
2020-08-19 09:16:47 +0800

    routing

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>home page</p>');
+    //res.send('<p>home page</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/about-us', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
 });
