a2e9cecc7248dfd7b738d4f869bfa6eab3a1eb3a
desperatecat <376198935@qq.com>
2020-08-22 09:47:32 +0800

    remove

--- app.js
+++ app.js
@@ -13,30 +13,19 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 //register view engine
 app.set('view engine', 'ejs');
 
-
-
 // middleware & static files
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
 app.use(morgan('dev'));
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
+
+
+
+
+
 
 app.get('/', (req, res) => {
     const blogs = [
