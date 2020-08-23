3643c00588a572117d6dd87308fe00ed8616fcd6
desperatecat <376198935@qq.com>
2020-08-23 11:32:44 +0800

    use new middleware

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use((req, res, next) => {
